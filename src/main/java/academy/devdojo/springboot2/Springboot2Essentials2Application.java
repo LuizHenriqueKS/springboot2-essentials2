8392d5da4e34db3f417de7ab0462865a97230ae0
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2021-12-05 11:09:34 -0300

    first commit

---
+++ src/main/java/academy/devdojo/springboot2/Springboot2Essentials2Application.java
@@ -0,0 +1,15 @@
+package academy.devdojo.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class Springboot2Essentials2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2Essentials2Application.class, args);
+	}
+
+}
