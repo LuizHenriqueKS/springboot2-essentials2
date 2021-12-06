fa812a0edc291e9223f3134bbcd620c15cdb9a42
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2021-12-06 06:15:14 -0300

    Spring Boot 2 Essentials 11 - Método PUT

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +39,23 @@ public class AnimeController {
     public ResponseEntity<Anime> findById(@PathVariable Long id) {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Anime> delete(@PathVariable Long id) {
+        animeService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Anime> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,6 +1,8 @@
 package academy.devdojo.springboot2.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,11 +15,11 @@ public class AnimeService {
 
     // private final AnimeRepository animeRepository;
 
-    private List<Anime> animes = List.of(
+    private List<Anime> animes = new ArrayList<>(List.of(
             new Anime(1L, "DBZ"),
             new Anime(2L, "Berserk"),
             new Anime(3L, "Naruto"),
-            new Anime(4L, "Boku no hero"));
+            new Anime(4L, "Boku no hero")));
 
     public List<Anime> listAll() {
         return animes;
@@ -28,6 +30,29 @@ public class AnimeService {
                 .filter(a -> a.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(getNewId());
+        animes.add(anime);
+        return anime;
+    }
+
+    private long getNewId() {
+        long biggestId = animes.stream()
+                .mapToLong(Anime::getId)
+                .max()
+                .orElse(0L);
+        return biggestId + 1;
+    }
+
+    public void delete(Long id) {
+        animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime) {
+        delete(anime.getId());
+        animes.add(anime);
     }
 
 }
