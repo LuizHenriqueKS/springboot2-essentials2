c21e871076532b1e07f07b18000502616bf1c4cc
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2021-12-10 13:29:22 -0300

    Spring Boot 2 Essentials 23 - Paginação

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -2,8 +2,8 @@ package academy.devdojo.springboot2.service;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,8 +21,8 @@ public class AnimeService {
 
     private final AnimeRepository animeRepository;
 
-    public List<Anime> listAll() {
-        return animeRepository.findAll();
+    public Page<Anime> listAll(Pageable pageable) {
+        return animeRepository.findAll(pageable);
     }
 
     public List<Anime> findByName(String name) {
