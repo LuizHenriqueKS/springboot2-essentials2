39cb966a642ccecc40661b64d30c7710d359ad78
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2021-12-11 12:01:43 -0300

    Spring Boot 2 Essentials 27 - RestTemplate exchange

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -1,5 +1,10 @@
 package academy.devdojo.springboot2.client;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,10 +14,18 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
-        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/2", Anime.class);
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/", Anime.class, 2);
         log.info(entity);
 
-        Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/2", Anime.class);
+        Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/", Anime.class, 2);
         log.info(object);
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+        log.info(Arrays.toString(animes));
+
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {
+                });
+        log.info(exchange.getBody());
     }
 }
